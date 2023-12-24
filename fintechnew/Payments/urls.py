3212b8a789de8bd9357fe83eb20400b2d852c25e
henry <aengrhenry@gmail.com>
2023-12-24 07:53:05 +0100

    integrated the vtpass api services for airtime, data, electricity bill and tv subscription

--- fintechnew/Payments/urls.py
+++ fintechnew/Payments/urls.py
@@ -7,9 +7,13 @@ urlpatterns = [
     path('service-provider/<str:category_id>/', views.service_provider_page, name='provider'),
     path('provider/<str:service_id>/', views.buy_page_view, name="buy-now"),
     path('verify_smartcard/', views.verify_smartcard, name='verify_smartcard'),
+    path('verify-meter/', views.verify_meter_number, name='electric-meter'),
     path('customer-confirm/', views.customer_comfirm_page, name="tv-customer"),
     path('change-plan/<str:service_id>/', views.change_tv_subcription_bouquet, name='tv-package'),
-    path('subscription_form/', views.subscription_form, name='subscription_form'),
+    path("renew-tv-subscription/", views.renew_tv_subscription_bouquet, name="tv-sub-renew"),
+    path('subscription/payment/', views.handle_airtime_or_data_subscription, name='mobile-payment'),
+    path('buy-electricity/', views.buy_electric_unit, name="buy-electricity"),
+    path("transaction-success/<str:request_id>/", views.transaction_successful, name='success')
 ]
 
 
--- fintechnew/Payments/views.py
+++ fintechnew/Payments/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 import requests
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from finweb.utils import get_vtpass_basic_auth, get_variations_code, verify_smartnumber
+from finweb import utils 
 from finweb.models import Category, NetworkService
 
 
@@ -23,13 +24,46 @@ def buy_page_view(request, service_id):
     provider=get_object_or_404(NetworkService, service_id=service_id)
     context={'provider':provider }
     if "data" in service_id:
-        variations=get_variations_code(service_id)
+        variations=utils.get_variations_code(service_id)
         data_plans=variations['content']['varations']
         context['data_plans']=data_plans
-    else:
-        print("no")
-    
     return render(request, "payments/payment.html", context)
+
+def handle_airtime_or_data_subscription(request):
+    if request.method == "POST":
+        phone=request.POST.get('phone_number')
+        amount=request.POST.get('amount')
+        service_id=request.POST.get('serviceid')
+        sub_plan=request.POST.get('plan_choice') #variation_code
+        req_id=utils.generate_request_id()
+        incoming_payload={
+            "req_id":req_id,
+            "service_id":service_id,
+            "amount":amount,
+            "sub_phone":phone,
+            "variation_code":sub_plan,
+            "phone_number":int('08011111111')
+        }
+        request.session['sev_id']=service_id
+        output_result=utils.pay_for_airtime_or_data_subscription(incoming_payload)
+        print(output_result)
+        request.session['reqID']=output_result['requestId']
+        return redirect("Payments:success", request_id=req_id)
+    get_service_id=request.session.pop('sev_id')
+    return redirect("Payments:buy-now", service_id=get_service_id)
+
+
+def transaction_successful(request, request_id):
+    get_transaction_status=utils.transaction_status(request_id)
+    print(get_transaction_status)
+    context={}
+    transaction=get_transaction_status['content']['transactions']
+    context['status_data']=transaction
+    if transaction['type'] == "Electricity Bill":
+        context['token']=get_transaction_status['purchased_code']
+        context['unit']=get_transaction_status['units']
+
+    return render(request, "payments/success.html", context)
 
 @login_required
 def verify_smartcard(request):
@@ -37,7 +71,23 @@ def verify_smartcard(request):
         smartcard_number = request.POST.get('smart_number')
         service_id = request.POST.get('serviceid')
         request.session['global_service_id']=service_id
-        customer=verify_smartnumber(smartcard_number, service_id)
+        customer=utils.verify_smartnumber(smartcard_number, service_id)
+        print('customer', customer)
+        customer_obj=customer['content']
+        request.session['customer']=customer_obj
+        return redirect("Payments:tv-customer") 
+    g_service_id=request.session.get('global_service_id')    
+    return redirect("Payments:buy-now", service_id=g_service_id)
+
+@login_required
+def verify_meter_number(request):
+    if request.method == 'POST':
+        meter_number = request.POST.get('meter_number')
+        meter_type = request.POST.get('meter_type_choice')
+        service_id = request.POST.get('serviceid')
+        request.session['global_service_id']=service_id
+        request.session['meter_type']=meter_type
+        customer=utils.meter_number_verify(meter_number,  meter_type, service_id)
         customer_obj=customer['content']
         request.session['customer']=customer_obj
         return redirect("Payments:tv-customer") 
@@ -50,88 +100,99 @@ def customer_comfirm_page(request):
     context={
         "customer":customer,
         "service_id":request.session.get('global_service_id')
+        
     }
     return render(request, "payments/customer-confirm.html", context)
 
 
 @login_required
 def change_tv_subcription_bouquet(request, service_id):
-    variations=get_variations_code(service_id)
+    variations=utils.get_variations_code(service_id)
     available_plan=variations['content']['varations']
-    print(available_plan)
     context={
         "package_plans":available_plan,
         "service_id":service_id
     }
+    if request.method == "POST":
+        variation_code = request.POST.get('variation_code')
+        card_num = request.POST.get('card_number')
+        phone = request.POST.get('phone')
+        amount= request.POST.get('amount')
+        sev_id=request.POST.get('serviceid')
+        req_id=utils.generate_request_id()
+        incoming_payload={
+            "request_id":req_id,
+            "service_id":sev_id,
+            "amount":amount,
+            "phone":phone,
+            "variation_code":variation_code,
+            "smartcard_number": card_num
+        }
+        result=utils.pay_for_tv_new_package_sub(incoming_payload)
+        print(result)
+        request.session['request_id']=result['requestId']
+        if result['response_description'] == "TRANSACTION SUCCESSFUL":
+            return redirect("Payments:success", request_id=req_id)
     return render(request, "payments/change_plan.html", context)
 
 
 @login_required
 def renew_tv_subscription_bouquet(request):
-    pass
+    customer=request.session.get('customer')
+    context={
+        "customer":customer,
+        "service_id":request.session.get('global_service_id')
 
-# def purchase_product(request):
-#     if request.method == 'POST':
-#         smartcard_number = request.POST.get('smartcard_number')
-#         variation_code = request.POST.get('variation_code')
-#         amount = request.POST.get('amount')
-#         service_id = 'dstv'
-#         request_id = 'your_unique_request_id'  # Generate a unique request ID
-
-#         purchase_url = f'https://sandbox.vtpass.com/api/pay'
-
-#         headers = {
-#             'Authorization': get_vtpass_basic_auth(),
-#         }
-
-#         data = {
-#             'request_id': request_id,
-#             'serviceID': service_id,
-#             'billersCode': smartcard_number,
-#             'variation_code': variation_code,
-#             'amount': amount,
-#             'phone': 'customer_phone_number',
-#             'subscription_type': 'renew',  # or 'change' for bouquet change
-#             'quantity': 1,
-#         }
-
-#         response = requests.post(purchase_url, headers=headers, data=data)
-
-#         if response.status_code == 200:
-#             api_response = response.json()
-#             # Process the API response and display relevant data to the user
-#             return render(request, 'purchase_product.html', {'api_response': api_response})
-#         else:
-#             error_message = "Error occurred while purchasing product"
-#             return render(request, 'purchase_product.html', {'error_message': error_message})
-        
-#     return HttpResponse("This is the purchase_product view.")
-
-
-def subscription_form(request):
-
-    bouquet_options = [
-
-        {"name": "DStv Padi", "value": "N2,500"},
-        {"name": "DStv Yanga", "value": "N3,500"},
-        {"name": "Dstv Confam", "value": "N6,200"},
-        {"name": "DStv Compact", "value": "N10,500"},
-        # Add more options as needed
-    ]
-    
-    context = {
-
-        "bouquet_options": bouquet_options,
     }
-    
-    if request.method == "POST":
-        selected_bouquet = request.POST.get("bouquet")
-        for option in bouquet_options:
-            if option["name"] == selected_bouquet:
-                context["selected_amount"] = option["value"]
-                break
+    # handle post req
+    if request.method == 'POST':
+        card_num = request.POST.get('smartcard_number')
+        phone = request.POST.get('phone')
+        amount= request.POST.get('amount')
+        sev_id=request.POST.get('service_id')
+        req_id=utils.generate_request_id()
+        incoming_payload={
+            "request_id":req_id,
+            "service_id":sev_id,
+            "amount":amount,
+            "phone":phone,
+            "smartcard_number": card_num
+        }
+        result=utils.pay_for_tv_subscription_renew(incoming_payload)
+        print(result)
+        request.session['request_id']=result['requestId']
+        if result['response_description'] == "TRANSACTION SUCCESSFUL":
+            return redirect("Payments:success", request_id=req_id)
+    return render(request, "payments/renews.html", context)
 
-    return render(request, "subscription_form.html", context)
+
+@login_required
+def buy_electric_unit(request): 
+    context={
+        "service_id":request.session.get('global_service_id'),      
+    }
+    # handle post req
+    if request.method == 'POST':
+        card_num = request.POST.get('meter_number')
+        phone = request.POST.get('phone')
+        amount= request.POST.get('amount')
+        sev_id=request.POST.get('service_id')
+        req_id=utils.generate_request_id()
+        incoming_payload={
+            "request_id":req_id,
+            "service_id":sev_id,
+            "amount":amount,
+            "phone":phone,
+            "meter_number": card_num,
+            "meter_type":request.session.get('meter_type')
+        }
+        result=utils.buy_electricity_api_call(incoming_payload)
+        request.session['request_id']=result['requestId']
+        if result['response_description'] == "TRANSACTION SUCCESSFUL":
+            return redirect("Payments:success", request_id=req_id)
+    return render(request, "payments/buy_electricity.html", context)
+
+
 
 
 
